f5c3c25f200a2693954bc26d284d4cd2acca6504
Gabriele Ferreira <mariagabrieleoliveira@gmail.com>
2024-05-09 15:14:34 -0300

    validating messages

--- cypress/e2e/bookshelf.cy.js
+++ cypress/e2e/bookshelf.cy.js
@@ -5,6 +5,7 @@ describe('Bookshelf tests', () => {
     cy.getAllBooks()
       .should((response)=>{
         expect(response.status).equal(200)
+        expect(response.body).to.have.property('Livros')
       })
   })
 
@@ -13,31 +14,36 @@ describe('Bookshelf tests', () => {
     cy.addBook(book, 
     "Teste8", 
     book, 
-    "2000-04-23", 
+    2000, 
     book
-    ).then((response)=>{
-      expect(response.status).equal(200)
-    })
-  })
-
-  it('should shown error message when creating book with already existing name', ()=> {
-    cy.addBook("Teste8", 
-    "Teste8", 
-    "Teste8", 
-    "2000-04-23", 
-    "Teste8"
     ).then((response)=>{
       const dec = new TextDecoder();
       //console.log(JSON.parse(dec.decode(response.body)))
       const responseJSON = JSON.parse(dec.decode(response.body))
       expect(response.status).equal(200)
-      expect(responseJSON.message).equal("Este livro já está no banco de dados.")
+      expect(responseJSON.message).equal("Livro adicionado com sucesso")
     })
   })
 
-  it.only('should delete book from the bookshelf', () => {
-    cy.deleteBook(25).then((response)=>{
+  it('should shown error message when creating book with already existing name', ()=> {
+    cy.addBook("Teste", 
+    "Teste", 
+    "Teste", 
+    2024, 
+    "Teste"
+    ).then((response)=>{
+      const dec = new TextDecoder();
+      //console.log(JSON.parse(dec.decode(response.body)))
+      const responseJSON = JSON.parse(dec.decode(response.body))
       expect(response.status).equal(200)
+      expect(responseJSON.message).equal("Esse livro já está no banco de dados")
+    })
+  })
+
+  it('should delete book from the bookshelf', () => {
+    cy.deleteBook(3).then((response)=>{
+      expect(response.status).equal(200)
+      expect(response.body.message).equal("Livro deletado com sucesso")
     })
   });
 })
