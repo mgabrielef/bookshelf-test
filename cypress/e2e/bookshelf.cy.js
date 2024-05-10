8149039de9fe5203078274e98a6f188af04be306
Gabriele Ferreira <mariagabrieleoliveira@gmail.com>
2024-05-10 14:49:43 -0300

    edit book

--- cypress/e2e/bookshelf.cy.js
+++ cypress/e2e/bookshelf.cy.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+/// <reference types='cypress' />
 
 describe('Bookshelf tests', () => {
   it('should return all books in the bookshelf', () => {
@@ -12,35 +12,52 @@ describe('Bookshelf tests', () => {
   it('should add book to the bookshelf', () => {
     let book = 'book ' + Math.floor(Math.random() * 100000)
     cy.addBook(book, 
-    "teste", 
+    'teste', 
     book, 
     2000, 
     book,
-    "teste",
+    'teste',
     100,
-    "teste"
+    'teste'
     ).then((response)=>{
       const dec = new TextDecoder();
       //console.log(JSON.parse(dec.decode(response.body)))
       const responseJSON = JSON.parse(dec.decode(response.body))
       expect(response.status).equal(200)
-      expect(responseJSON.message).equal("Livro adicionado com sucesso")
+      expect(responseJSON.message).equal('Livro adicionado com sucesso')
     })
   })
 
-  it.only('should shown error message when creating book with already existing name', ()=> {
-    cy.addBook("teste", "teste", "teste", 2000, "teste", "teste", 100, "teste")
+  it('should shown error message when creating book with already existing name', ()=> {
+    cy.addBook('teste', 'teste', 'teste', 2000, 'teste', 'teste', 100, 'teste')
       .then((response)=>{
         let dec = new TextDecoder();
         //console.log(JSON.parse(dec.decode(response.body)))
         let responseJSON = JSON.parse(dec.decode(response.body))
         expect(response.status).equal(400)
-        expect(responseJSON.message).equal("Esse livro já está no banco de dados")
+        expect(responseJSON.message).equal('Esse livro já está no banco de dados')
       })
   })
 
+  it.only('should edit book\'s info', () => {
+    let book = 'book ' + Math.floor(Math.random() * 100000)
+    cy.addBook(book, 'test', book, 2000, book, 'test', 100, 'test')
+      .then(response=>{
+        let dec = new TextDecoder();
+        let responseJSON = JSON.parse(dec.decode(response.body))
+        let id = responseJSON.book.id
+
+        cy.editBook(id, 'edited ' + book, 'test', 'edited ' + book, 2000, 'edited ' + book, 'test', 100, 'test')
+          .then((response)=>{
+            expect(response.status).equal(200)
+            expect(response.body.message).equal('Livro atulizado com sucesso')
+          })
+      })
+  });
+
+
   it('should delete book from the bookshelf', () => {
-    cy.addBook("book to delete", "test", "test", 2000, "test", "test", 100, "test")
+    cy.addBook('book to delete', 'test', 'test', 2000, 'test', 'test', 100, 'test')
       .then(response=>{
         let dec = new TextDecoder();
         let responseJSON = JSON.parse(dec.decode(response.body))
@@ -48,7 +65,7 @@ describe('Bookshelf tests', () => {
 
         cy.deleteBook(id).then((response)=>{
           expect(response.status).equal(200)
-          expect(response.body.message).equal("Livro deletado com sucesso")
+          expect(response.body.message).equal('Livro deletado com sucesso')
         })
       })
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,27 +1,27 @@
 Cypress.Commands.add('getAllBooks', ()=>{
     cy.request({
-        method: "GET",
-        url: "api/book"
+        method: 'GET',
+        url: 'api/book'
     })
 })
 
 Cypress.Commands.add('addBook', (name, description, author, year, publisher, genre, pagecount, lang)=>{
     cy.fixture('teste1.png', 'binary').then( image =>{
-        const blob = Cypress.Blob.binaryStringToBlob(image, "image/png")
+        const blob = Cypress.Blob.binaryStringToBlob(image, 'image/png')
         const formData = new FormData()
-        formData.append("name", name)
-        formData.append("description", description)
-        formData.append("author", author)
-        formData.append("year", year)
-        formData.append("image", blob, 'teste1.png')
-        formData.append("publisher", publisher)
-        formData.append("genre", genre)
-        formData.append("pagecount", pagecount)
-        formData.append("lang", lang)
+        formData.append('name', name)
+        formData.append('description', description)
+        formData.append('author', author)
+        formData.append('year', year)
+        formData.append('image', blob, 'teste1.png')
+        formData.append('publisher', publisher)
+        formData.append('genre', genre)
+        formData.append('pagecount', pagecount)
+        formData.append('lang', lang)
 
         cy.request({
-            method: "POST",
-            url: "api/book",
+            method: 'POST',
+            url: 'api/book',
             headers: {
                 'content-type': 'multipart/form-data',
             },
@@ -31,9 +31,26 @@ Cypress.Commands.add('addBook', (name, description, author, year, publisher, gen
     })
 })
 
+Cypress.Commands.add('editBook', (id, name, description, author, year, publisher, genre, pagecount, lang)=>{   
+    cy.request({
+        method: 'PATCH',
+        url: `api/book/${id}`,
+        body: {
+            "name": name,
+            "description": description,
+            "author": author,
+            "year": year,
+            "publisher": publisher,
+            "genre": genre,
+            "pagecount": pagecount,
+            "lang": lang
+        }
+    })
+})
+
 Cypress.Commands.add('deleteBook', (id)=>{
     cy.request({
-        method: "DELETE",
+        method: 'DELETE',
         url: `api/book/${id}`
     })
 })
